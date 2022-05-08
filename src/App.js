be426e7ea315bf10ec2ee63e5cd46d501b3d902b
cal <caldayham@gmail.com>
2022-05-07 20:29:15 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Mel Day is Epic
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://wallofsongproject.com/mel-day/#:~:text=Mel%20Day%20is%20a%20SF,The%20Wall%20of%20Song%20Project."
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Check her out!
         </a>
       </header>
     </div>
